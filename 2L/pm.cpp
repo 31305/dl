56c30d9717fa95f120e731789d2281a24a64b5b0
- <>
2025-07-26 13:52:47 +0530

    -

--- 2L/pm.cpp
+++ 2L/pm.cpp
@@ -1,4 +1,21 @@
+#include<emscripten/emscripten.h>
+#include<jsdp.h>
+#include<dv.h>
+#include<GLES2/gl2.h>
+dv dvs;
+bool pd(double,void*)
+{
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+	dv::kvsl k1={ -0.8f, -0.5f, 0.9f, 0.9f, 1.0f, 0.0f, 0.0f, 0.5f };
+	dvs.cl(k1);
+	return 0;
+}
 int main()
 {
+	jsdp dp;
+	dp.d();
+	dvs.dk();
+    emscripten_request_animation_frame(pd,0);
 	return 0;
 }
--- jsdp.h
+++ jsdp.h
@@ -11,7 +11,7 @@ struct jsdp
 			let jsdp=document.createElement('canvas');
 			jsdp.id='jsdp';
 			jsdp.style="position:absolute;display:block;width:100%;height:100%";
-			jsdp.oncontextmenu="event.preventDefault()"
+			jsdp.oncontextmenu="event.preventDefault()";
 			jsdp.width=0;
 			jsdp.height=0;
 			document.body.appendChild(jsdp);
@@ -24,7 +24,7 @@ struct jsdp
 		dpps.antialias=EM_TRUE;
 		dpps.majorVersion=1;
 		dpps.minorVersion=0;
-		ps=emscripten_webgl_create_context("jsdp",&dpps);
+		ps=emscripten_webgl_create_context("#jsdp",&dpps);
 		emscripten_webgl_make_context_current(ps);
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
