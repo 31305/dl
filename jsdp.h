fc42c267c4a6f1daad8c29a8386995c0f4c7354d
- <>
2025-07-27 14:45:30 +0530

    -

--- jsdp.h
+++ jsdp.h
@@ -11,6 +11,7 @@ struct jsdp
 	bool db=0;
 	std::vector<std::function<void()>> vppk;
 	std::vector<std::function<void(double)>> sck;
+	std::vector<std::function<void(int,bool,double,double)>> ndk;
 	int vpv1,vpv2;
 	void dk()
 	{
@@ -82,6 +83,19 @@ EMSCRIPTEN_KEEPALIVE inline void jsdpvp(size_t p,int v1,int v2)
 		emscripten_webgl_make_context_current(s->ps);
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+		EM_ASM({
+			function k(p,kn)
+			{
+				let s=document.body.getBoundingClientRect();
+				Module.ccall('jsdpnd',null,['number','number','number','number','number'],
+					[$0,kn,(p.pressure||(p.buttons==1))?true:false,p.clientX/s.width,p.clientY/s.height]);
+				console.log(p.buttons);
+			}
+			document.body.addEventListener("pointermove",(p)=>k(p,1));
+			document.body.addEventListener("pointerdown",(p)=>k(p,2));
+			document.body.addEventListener("pointerout",(p)=>k(p,3));
+			document.body.addEventListener("pointerup",(p)=>k(p,4));
+		},s);
 		if(s->pk)s->pk();
 	}
 	else
@@ -91,5 +105,12 @@ EMSCRIPTEN_KEEPALIVE inline void jsdpvp(size_t p,int v1,int v2)
 			if(s->vppk[k])s->vppk[k]();
 	}
 	s->cnr();
+}
+EMSCRIPTEN_KEEPALIVE inline void jsdpnd(void *p,int kn,bool s,double s1,double s2)
+{
+	jsdp* m=(jsdp*)p;
+	for(size_t k=0;k<m->ndk.size();k++)
+		if(m->ndk[k])
+			m->ndk[k](kn,s,s1*m->vpv1,s2*m->vpv2);
 }
 }
--- nd.h
+++ nd.h
@@ -6,7 +6,7 @@ struct nd
 	jsdp *dp;
 	bool n=0;
 	int ms1,ms2,ns1,ns2;
-	size_t ckk;
+	size_t ckk,ndkk;
 	std::vector<dv::kvsl> vs;
 	bool sd,kd;
 	int s=0;
@@ -15,19 +15,8 @@ struct nd
 		vs.reserve(9);
 		ckk=dp->sck.size();
 		dp->sck.push_back([this](double kn){ck(kn);});
-		EM_ASM({
-			function k(p,kn)
-			{
-				let s=document.body.getBoundingClientRect();
-				Module.ccall('ndck',null,['number','number','number','number','number'],
-					[$0,kn,(p.pressure||(p.buttons==1))?true:false,p.clientX/s.width,p.clientY/s.height]);
-				console.log(p.buttons);
-			}
-			document.body.addEventListener("pointermove",(p)=>k(p,1));
-			document.body.addEventListener("pointerdown",(p)=>k(p,2));
-			document.body.addEventListener("pointerout",(p)=>k(p,3));
-			document.body.addEventListener("pointerup",(p)=>k(p,4));
-		},this);
+		ndkk=dp->ndk.size();
+		dp->ndk.push_back([this](int kn,bool s,double s1,double s2){ndck(kn,s,s1,s2);});
 	}
 	void ck(double kn)
 	{
@@ -35,36 +24,34 @@ struct nd
 	~nd()
 	{
 		dp->sck[ckk]=0;
-	}
-};
-extern "C"
-{
-EMSCRIPTEN_KEEPALIVE inline void ndck(void *p,int kn,bool s,double s1,double s2)
-{
-	nd* n=(nd*)p;
-	if(!n->n)
+		dp->ndk[ndkk]=0;
+	};
+	void ndck(int kn,bool s,double s1,double s2)
 	{
-		if(kn==2)
+		if(!n)
 		{
-			n->n=1;
-			n->ms1=s1*(double)n->dp->vpv1;
-			n->ms2=s2*(double)n->dp->vpv2;
-			n->ns1=n->ms1;
-			n->ns2=n->ms2;
-		}
-	}
-	else
-	{
-		if(kn==3||kn==4||!s)
-		{
-			n->n=0;
-			if(n->s)n->dp->cnr();
+			if(kn==2)
+			{
+				n=1;
+				ms1=s1;
+				ms2=s2;
+				ns1=s1;
+				ns2=s2;
+				s=0;
+			}
 		}
 		else
 		{
-			n->ns1=s1*(double)n->dp->vpv1;
-			n->ns2=s2*(double)n->dp->vpv2;
+			if(kn==3||kn==4||!s)
+			{
+				n=0;
+				if(s)dp->cnr();
+			}
+			else
+			{
+				ns1=s1;
+				ns2=s2;
+			}
 		}
 	}
-}
-}
+};
