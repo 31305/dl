0227e2bcca19ef60d618699db86b6afc6531a4e9
- <>
2025-07-29 07:03:23 +0530

    -

--- 2L/pm.cpp
+++ 2L/pm.cpp
@@ -16,8 +16,8 @@ struct jstp
 			dvs.dk();
 			dp.sck.push_back([this](double)
 			{
-				int vv=10*((std::min(dp.vpv1,dp.vpv2)/9)/10);
-				dv::kvsl v={(dp.vpv1-vv)/2,(dp.vpv2-vv)/2,vv,vv,0.2578125,0.00390625,0.19140625,1.0};
+				int vv=10*((std::min(dp.vpv1,dp.vpv2)/7)/10);
+				dv::kvsl v={(dp.vpv1-vv)/2,(dp.vpv2-vv)/2,vv,vv,.4,0,.4,1};
 				dv::kvsl dv={(dp.vpv1-vv)/2+vv/10,(dp.vpv2-vv)/2+vv/10,vv*4/5,vv*4/5,0,0,0,1.0};
 				dvs.cl({v,dv},dp.vpv1,dp.vpv2);
 			});
