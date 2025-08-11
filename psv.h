d494e812087ad7cf6f08309d06e02e08b04ea12a
- <>
2025-08-11 11:28:46 +0530

    -

--- psv.h
+++ psv.h
@@ -8,11 +8,32 @@ namespace psv
 		const std::vector<std::vector<std::vector<vk::v>>> ps;
 		psv(const decltype(ps) &p):ps(p){};
 		size_t s=0;
+		bool kp=0;
 		std::vector<std::vector<vk::v>> k(int p)override
 		{
-			if(p==-4)return {};
-			auto ps=(p==-3?vk::vs({70,2}):p==-2?vk::vs({51,8,75}):vk::vs({5,75}));
-			return {ps,vk::vs({46,5,56,3,51,1,75})};
+			if(p==-3)
+			{
+				return {vk::vs({70,2})};
+			}
+			else if(p==-2)
+			{
+				kp=1;
+				std::vector<std::vector<vk::v>> tp={vk::vs({51,8,75})};
+				tp.insert(tp.end(),ps[s].begin(),ps[s].end());
+				return tp;
+			}
+			else if(p==-1)
+			{
+				std::vector<std::vector<vk::v>> tp={vk::vs({5,75})};
+				if(kp)
+				{
+					s+=1;
+					if(s==ps.size()){s=0;kp=0;}
+					else tp.insert(tp.end(),ps[s].begin(),ps[s].end());
+				}
+				return tp;
+			}
+			return {};
 		}
 	};
 	inline std::vector<std::vector<std::vector<std::vector<vk::v>>>> pss()
