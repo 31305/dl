4b6b4e766f8e500e6d5710e92ac5cdbb89b752a3
- <>
2025-08-20 12:50:57 +0530

    -

--- 2L/vsg.h
+++ 2L/vsg.h
@@ -1,6 +1,12 @@
 #pragma once
 #include<vp.h>
+#include<emscripten/emscripten.h>
+#include<string>
+inline std::function<void()> njsps(const std::string &nd)
+{
+	return [nd](){EM_ASM({window.location.href=UTF8ToString($0);},nd.c_str());};
+}
 inline svvs vsg()
 {
-	return {{{51,8,76,45,3,51,48,43,1,66,31}},{{{{51,1,67,5}}},{{{47,4,49,66,44,2,75}}}}};
+	return {{{51,8,76,45,3,51,48,43,1,66,31}},{{{{49,1,76,53,2,65,3,51,1,75}},{},njsps("https://amzn.to/470ggKD")}}};
 }
