150d4c3cf1d8be16658f112f692fdf7db713e02a
- <>
2025-11-11 12:07:49 +0530

    -

--- sksl/m.cpp
+++ sksl/m.cpp
@@ -1,10 +1,17 @@
 #include<vk/vk.h>
+#ifdef PVL
 #include<sndfile.h>
+#endif
 #include<iostream>
 bool vksvl=0;
 void lk(void* nv,float l)
 {
-	if(nv)sf_write_float((SNDFILE*)nv,&l,1);
+	if(nv)
+	{
+#ifdef PVL
+		sf_write_float((SNDFILE*)nv,&l,1);
+#endif
+	}
 	else
 	{
 		fwrite(&l,sizeof(l),1,stdout);
@@ -15,12 +22,14 @@ int main(int nds,char* nd[])
 {
 	vk::vks m;
 	m.smg=1;
+	void *tp=0;
+#ifdef PVL
 	SF_INFO vv;
 	vv.samplerate=m.mt.outputSampleRate();
 	vv.channels=1;
 	vv.format=SF_FORMAT_WAV|SF_FORMAT_FLOAT;
-	SNDFILE* tp=0;
-	if(nds<1)tp=sf_open(nd[1],SFM_WRITE,&vv);
+	if(nds>1)tp=sf_open(nd[1],SFM_WRITE,&vv);
+#endif
 	if(!tp)
 	{
 		uint32_t dl[7]={0x2e736e64,28,0xffffffff,6,(uint32_t)m.mt.outputSampleRate(),1};
@@ -40,6 +49,8 @@ int main(int nds,char* nd[])
 			m.pmb(p,lk,tp);
 		}
 	}
-	if(tp)sf_close(tp);
+#ifdef PVL
+	if(tp)sf_close((SNDFILE*)tp);
+#endif
 	return 0;
 }
