f9b38ec38eac4a4ccca40eba580cfd4eedd90328
- <>
2025-12-04 12:18:59 +0530

    -

--- kl.py
+++ kl.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
+def v(s):
+    from os import system
+    for l in s:
+        system('echo '+' '.join([str(n) for n in l])+'|sksl|sox -t au - -d 2>/dev/null')
 def k():
     import curses
     curses.initscr()
     import sys
-    from os import system
-    j={0:()}
-    js=0
     s=[]
     while True:
         p=sys.stdin.read(1)
         if p=='n':
             break
-        if p==' ':
-            for l in [[51,8,75]]+s:
-                system('echo '+' '.join([str(n) for n in l])+'|sksl|sox -t au - -d 2>/dev/null')
+        if p=='k':
+            v([[51,8,75]]+s);
     curses.endwin()
 if __name__=='__main__':
     k()
