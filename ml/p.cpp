1528579bdc3b2a5ed0aec94e5f7387db090ee921
- <>
2025-12-06 15:06:29 +0530

    -

--- ml/p.cpp
+++ ml/p.cpp
@@ -5,11 +5,18 @@ void pk()
 	auto d=pd->getSceneManager();
 	auto b=d->addCubeSceneNode();
 	b->setMaterialFlag(irr::video::EMF_LIGHTING,false);
-	auto n=d->addCameraSceneNodeFPS();
+	auto n=d->addCameraSceneNodeFPS(0,100,.3,0,0,0,true,3);
 	n->setPosition(irr::core::vector3df(0,0,100));
 	n->setTarget(irr::core::vector3df(0,0,0));
 	auto k=pd->getVideoDriver();
 	auto p=pd->getGUIEnvironment();
+	irr::scene::ITriangleSelector* c;
+	c=d->createTriangleSelector(b->getMesh(),b);
+	b->setTriangleSelector(c);
+	auto g=d->createCollisionResponseAnimator(c,n);
+	n->addAnimator(g);
+	g->drop();
+
 	while(pd->run())
 	{
 		k->beginScene(true,true,0);
