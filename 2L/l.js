ddd98c54297973de85b546f008e173019eb71c06
- <>
2025-12-22 05:21:15 +0000

    -

--- 2L/l.js
+++ 2L/l.js
@@ -1,9 +1,9 @@
-function jdv()
+const jdv=function()
 {
 	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){return true;}
     return false;
 }
-class vp
+const vp=class
 {
 	constructor(pk=null)
 	{
@@ -14,7 +14,7 @@ class vp
 		if(this.p!=undefined)this.p.ccall('jb',null,['array','number'],[new Uint8Array(v),v.length]);
 	}
 }
-const v=new vp();
+const v=new vp(()=>{ssk();});
 d=document.createElement('canvas')
 document.body.style.margin='0';
 document.body.style.touchAction='none';
@@ -30,13 +30,6 @@ l.minZ=0.1;
 const p=new BABYLON.PointLight("p",new BABYLON.Vector3(0,0,0),s);
 p.intensity=2;
 p.parent=l;
-if(0)
-{
-	const dp=new BABYLON.SpotLight("dp",new BABYLON.Vector3(0,0,0),new BABYLON.Vector3(0,0,1),0.02,2,s);
-	dp.diffuse=new BABYLON.Color3(1,0.5,0.5);
-	dp.intensity=10;
-	dp.parent=l;
-}
 function bnm()
 {
 	const bv=10;
@@ -90,65 +83,70 @@ l.keysDown.push(83);
 l.keysRight.push(68);
 l.keysLeft.push(65);
 if(1)l.angularSensibility*=-1;
-d.addEventListener("click",(p)=>
+const ssk=function()
 {
-	if(!jdv()){if(document.pointerLockElement!=d){d.requestPointerLock();return;}}
-	else if(document.fullscreenElement!=d){d.requestFullscreen();screen.orientation.lock("landscape-primary");return;}
-	let n=s.pick(d.width/2,d.height/2).pickedMesh;
-	if(n==undefined)v.b([1,70,66,2,44,9,51,48,1,77]);
-	else
+	d.addEventListener("click",(p)=>
 	{
-		let p=n;
-		while(p!=undefined)
-		{
-			if(p.n!=undefined){v.b(n.n);return;}
-			else p=p.parent;
-		}
-	}
-});
-const vg=1;
-d.addEventListener("mousemove",(p)=>{if(document.pointerLockElement===d)
-{
-	l.cameraRotation.y+=p.movementX/d.height*vg; 
-	l.cameraRotation.x+=p.movementY/d.height*vg;
-}
-});
-const sss=new Map();
-d.addEventListener("touchstart",(p)=>
-{
-	for(const s of p.changedTouches)
-		if(!sss.has(s.identifier))
-			sss.set(s.identifier,{s1:s.clientX,s2:s.clientY,m:s.clientX/d.offsetWidth>0.5});
-});
-d.addEventListener("touchend",(p)=>
-{
-	for(const s of p.changedTouches)
-		sss.delete(s.identifier);
-});
-d.addEventListener("touchcancel",(p)=>
-{
-	for(const s of p.changedTouches)
-		sss.delete(s.identifier);
-});
-d.addEventListener("touchmove",(p)=>{if(document.fullscreenElement==d)
-{
-	for(const s of p.changedTouches)
-	{
-		let g1=(s.clientX-sss.get(s.identifier).s1)/Math.min(d.width,d.height)*vg;
-		let g2=(s.clientY-sss.get(s.identifier).s2)/Math.min(d.width,d.height)*vg;
-		if(sss.get(s.identifier).m)
-		{
-			l.cameraRotation.y-=g1; 
-			l.cameraRotation.x-=g2;
-		}
+		if(!jdv()){if(document.pointerLockElement!=d){d.requestPointerLock();return;}}
+		else if(document.fullscreenElement!=d){d.requestFullscreen();screen.orientation.lock("landscape-primary");return;}
+		let n=s.pick(d.width/2,d.height/2).pickedMesh;
+		if(n==undefined)v.b([1,70,66,2,44,9,51,48,1,75]);
 		else
-			l.cameraDirection.addInPlace(BABYLON.Vector3.TransformCoordinates
-				(new BABYLON.Vector3(-g1,0,g2),BABYLON.Matrix.RotationY(l.rotation.y)).scale(10));
-		sss.get(s.identifier).s1=s.clientX;
-		sss.get(s.identifier).s2=s.clientY;
+		{
+			let p=n;
+			while(p!=undefined)
+			{
+				if(p.n!=undefined){v.b(n.n);return;}
+				else p=p.parent;
+			}
+		}
+	});
+	const vg=1;
+	d.addEventListener("mousemove",(p)=>{if(document.pointerLockElement===d)
+	{
+		l.cameraRotation.y+=p.movementX/d.height*vg; 
+		l.cameraRotation.x+=p.movementY/d.height*vg;
 	}
+	});
+	const sss=new Map();
+	d.addEventListener("touchstart",(p)=>
+	{
+		for(const s of p.changedTouches)
+			if(!sss.has(s.identifier))
+				sss.set(s.identifier,{s1:s.clientX,s2:s.clientY,m:s.clientX/d.offsetWidth>0.5});
+	});
+	d.addEventListener("touchend",(p)=>
+	{
+		for(const s of p.changedTouches)
+			sss.delete(s.identifier);
+	});
+	d.addEventListener("touchcancel",(p)=>
+	{
+		for(const s of p.changedTouches)
+			sss.delete(s.identifier);
+	});
+	d.addEventListener("touchmove",(p)=>{if(document.fullscreenElement==d)
+	{
+		for(const s of p.changedTouches)
+		{
+			let g1=(s.clientX-sss.get(s.identifier).s1)/Math.min(d.width,d.height)*vg;
+			let g2=(s.clientY-sss.get(s.identifier).s2)/Math.min(d.width,d.height)*vg;
+			if(sss.get(s.identifier).m)
+			{
+				l.cameraRotation.y-=g1; 
+				l.cameraRotation.x-=g2;
+			}
+			else
+				l.cameraDirection.addInPlace(BABYLON.Vector3.TransformCoordinates
+					(new BABYLON.Vector3(-g1,0,g2),BABYLON.Matrix.RotationY(l.rotation.y)).scale(10));
+			sss.get(s.identifier).s1=s.clientX;
+			sss.get(s.identifier).s2=s.clientY;
+		}
+	}
+	});
+	c.runRenderLoop(()=>s.render());
+	window.addEventListener("resize",()=>{c.resize();});
 }
-});
 const ppd=BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("PPD",true,s);
 function lds()
 {
@@ -171,5 +169,3 @@ function lds()
 	ppd.addControl(ldd);
 }
 lds();
-c.runRenderLoop(()=>s.render());
-window.addEventListener("resize",()=>{c.resize();});
