e946855f09964bcbb7fbc9ffd3aa37e9262d93b3
- <>
2026-03-30 10:49:54 +0530

    -

--- sb/p.py
+++ sb/p.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python3
 import asyncio
 import sys
 import uuid
 import json
+from os import system
 from supabase import acreate_client
 
 sn = "ps"
 pn = str(uuid.uuid4())[:6]
-
 
 async def k():
     sv= await acreate_client("https://wgjgbevxmdkhadvfnrco.supabase.co","sb_publishable_wkMomrBAGwVqd8pGb6hDDA_N5q2uUS7")
@@ -15,10 +16,9 @@ async def k():
         s=p['data']['record']
 
         if s["sn"] == sn and s["pn"] != pn:
-            print(f"\n[{s['pn']}] {s['l']}")
-            print("> ", end="", flush=True)
+            system('echo '+s['l']+'| '+sksl+' | '+ss+' -t au - -d 2>/dev/null')
 
-    g= sv.channel("realtime:sg")
+    g=sv.channel("realtime:sg")
 
     g.on_postgres_changes(
     event="INSERT",
@@ -34,18 +34,35 @@ async def k():
     print("> ", end="", flush=True)
 
     sk= asyncio.get_event_loop()
-
+    
+    import os
+    import sys
+    global lsk
+    lsks=os.path.dirname(lsk)
+    sys.path.append(lsks)
+    import lsk
     while True:
         l = await sk.run_in_executor(None, sys.stdin.readline)
         pl = l.strip()
-
         if pl:
             await sv.table("sg").insert({
                 "sn": sn,
                 "pn": pn,
-                "l": pl
+                "l":' '.join([str(s) for s in lsk.snl(pl)])
             }).execute()
 
 
 if __name__ == "__main__":
-    asyncio.run(k())
+    global sksl,lsk,ss
+    import shutil
+    sksl=shutil.which('sksl')
+    lsk=shutil.which('lsk.py')
+    ss=shutil.which('sox')
+    if sksl==None:
+        print('sksl?')
+    elif lsk==None:
+        print('lsk.py?')
+    elif ss==None:
+        print('sox?')
+    else:
+        asyncio.run(k())
