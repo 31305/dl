0ca76efc4b34e4724567df86eacd88aa04544567
- <>
2026-06-14 07:30:08 +0530

    -

--- 2L/js.py
+++ 2L/js.py
@@ -4,15 +4,10 @@ import os
 from os import system
 ls='./rs'
 system('mkdir -p '+ls)
-system('cp 2L.html '+ls+'/index.html')
-system('cp ss.js '+ls)
-system('cp 2L.svg '+ls)
-system('cp sks '+ls)
-system('cp 2L.json '+ls)
-system('cp 2L.js '+ls)
-system('cp 2L.wasm '+ls)
-system('cp 3.js '+ls)
-system('cp l.js '+ls)
+s=['2L.html','ss.js','2L.svg','sks','2L.json','2L.js','2L.wasm','3.js','l.js',
+   'babylon.js','babylon.gui.min.js','babylonjs.materials.min.js','HavokPhysics_umd.js',
+    'HavokPhysics.wasm','_headers','vm.js','vm.wasm','nlv.js','zxing_reader.js',
+   'zxing_reader.wasm','4.js','5.js']
 if not(os.path.isfile('babylon.js') and os.path.isfile('babylon.gui.min.js') and os.path.isfile('babylonjs.materials.min.js')):
     system('wget -O lk.zip "https://github.com/BabylonJS/Babylon.js/releases/download/8.40.1/cdnSnapshot.zip"')
     system('unzip lk.zip && rm lk.zip')
@@ -21,15 +16,11 @@ if not(os.path.isfile('babylon.js') and os.path.isfile('babylon.gui.min.js') and
     system('cp .snapshot/gui/babylonjs.materials.min.js ./')
     system('cp .snapshot/gui/babylon.gui.min.js ./')
     system('cp .snapshot/havok/* ./')
-system('cp babylon.js '+ls)
-system('cp babylon.gui.min.js '+ls)
-system('cp babylonjs.materials.min.js '+ls)
-system('cp HavokPhysics_umd.js '+ls)
-system('cp HavokPhysics.wasm '+ls)
-system('cp _headers '+ls)
-system('cp vm.js '+ls)
-system('cp vm.wasm '+ls)
-system('cp nlv.js '+ls)
-system('cp zxing_reader.js '+ls)
-system('cp zxing_reader.wasm '+ls)
-system('cp 4.js '+ls)
+import sys
+if len(sys.argv)>1 and sys.argv[1]=='j':
+    for p in s:
+        system('wget -O '+p+' 1007.in/'+p)
+for p in s:
+    system('cp '+p+' '+ls)
+system('mv '+ls+'/2L.html '+ls+'/index.html')
+
