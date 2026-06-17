4431dadb2a7a272b0ff1c3ba848b1062b1219f70
- <>
2026-06-17 17:32:27 +0530

    -

--- 2L/js.py
+++ 2L/js.py
@@ -7,7 +7,7 @@ system('mkdir -p '+ls)
 s=['2L.html','ss.js','2L.svg','sks','2L.json','2L.js','2L.wasm','3.js','l.js',
    'babylon.js','babylon.gui.min.js','babylonjs.materials.min.js','HavokPhysics_umd.js',
     'HavokPhysics.wasm','_headers','vm.js','vm.wasm','nlv.js','zxing_reader.js',
-   'zxing_reader.wasm','4.js','5.js']
+   'zxing_reader.wasm','4.js','5.js','6.js']
 if not(os.path.isfile('babylon.js') and os.path.isfile('babylon.gui.min.js') and os.path.isfile('babylonjs.materials.min.js')):
     system('wget -O lk.zip "https://github.com/BabylonJS/Babylon.js/releases/download/8.40.1/cdnSnapshot.zip"')
     system('unzip lk.zip && rm lk.zip')
@@ -23,4 +23,4 @@ if len(sys.argv)>1 and sys.argv[1]=='j':
 for p in s:
     system('cp '+p+' '+ls)
 system('mv '+ls+'/2L.html '+ls+'/index.html')
-
+system('mv '+ls+'/6.js '+ls+'/5.js')
