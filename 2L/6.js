d3cbd7c844fb991461a25188034ae593b5a83a8a
- <>
2026-06-17 13:46:08 +0530

    -

--- 2L/6.js
+++ 2L/6.js
@@ -35,16 +35,29 @@ const vp=class
 class jp
 {
 	constructor(){this.sbs=new Map();}
-	vs(v){if(!this.sbs.has(v))this.sbs.set(v,new Set())}
-	sbn(v1,v2)
+	vs(v){if(!this.sbs.has(v))this.sbs.set(v,{sb:new Set()})}
+	s(v1,v2)
 	{
 		this.vs(v1);
 		this.vs(v2);
-		this.sbs.get(v1).add(v2);
-		this.sbs.get(v2).add(v1);
+		this.sbs.get(v1).sb.add(v2);
+		this.sbs.get(v2).sb.add(v1);
 	}
-	sb(v){return this.sbs.get(v)||new Set();}
+	n(v,p)
+	{
+		this.vs(v);
+		this.sbs.get(v).n=p;
+	}
+	p(v){return this.sbs.get(v);}
 }
+const j=new jp();
+const jnm=()=>
+{
+	j.n(7206969,[70,7,75,73,16,44,1,49,2,77])
+	j.n(1093742,[70,7,75,73,16,72,1,45,2,75])
+	j.s(7206969,1093742)
+}
+jnm();
 const vsgk=(s,v1,v2,vk)=>
 {
 	let v=0;
@@ -66,20 +79,42 @@ const vsp=(s)=>
 	document.body.style.setProperty('--pl',(plm*vv).toString()+'px');
 	document.body.style.setProperty('--vk',(v*vkm).toString()+'px');
 	document.body.style.setProperty('--v',vv.toString()+'px');
+	return vv;
 }
 const sg=document.createElement('div');
 document.body.appendChild(sg);
-const ssk=(s)=>
+var s=1;
+const ssk=(k)=>
 {
-	for(let k=0;k<s;k++)
+	s=1+j.p(k).sb.size
+	const v=vsp(s);
+	const ss=(k)=>
 	{
-		const p=document.createElement('div');
-		p.className='nv';
-		sg.appendChild(p);
+		const ps=document.createElement('div');
+		ps.className='nv';
+		sg.appendChild(ps);
+		fetch('https://www.wikidata.org/wiki/Special:EntityData/Q'+k.toString()+'.json').then(p=>p.json()).then(p=>
+		{
+			const n=p.entities['Q'+k.toString()].claims.P18[0].mainsnak.datavalue.value.replace(' ', '_')
+			fetch('https://commons.m.wikimedia.org/w/api.php?action=query&titles=File:'+n
+				+'&prop=imageinfo&iiprop=url&iiurlwidth='
+				+Math.floor(v*window.devicePixelRatio).toString()+
+				'&format=json&formatversion=2&origin=*').then(p=>p.json()).then(p=>
+				{
+					const d=document.createElement('img')
+					const m=(0.95*v).toString()+'px';
+					d.style.width=m;
+					d.style.height=m;
+					d.crossOrigin="anonymous";
+					d.src=p.query.pages[0].imageinfo[0].thumburl;
+					ps.appendChild(d);
+				})
+		})
 	}
-	vsp(s);
+	ss(k)
 }
-window.tp=ssk
+window.onresize=()=>{vsp(s)}
+window.tp=ssk;
 ss('vm.js').then(p=>{
 	const v=new vp();
 	v.dk().then(p=>{
@@ -89,7 +124,8 @@ ss('vm.js').then(p=>{
 			border:var(--pl) solid var(--nvv,black);
 			margin-left:var(--vk);
 			margin-top:var(--vk);
-			display:inline-block;
+			display:grid;
+			place-items:center;
 			width:var(--v);
 			height:var(--v);
 		}
