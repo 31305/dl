dc8daddfac3cd143d030d82463b22f84fbdbf62f
- <>
2026-06-22 09:40:56 +0530

    -

--- 2L/6.js
+++ 2L/6.js
@@ -10,7 +10,16 @@ const ss=(n)=>
 		document.body.appendChild(v);
 	});
 }
-ss('https://unpkg.com/maplibre-gl@^5.24.0/dist/maplibre-gl.js').then(p=>
+Promise.all([ss('https://unpkg.com/maplibre-gl@^5.24.0/dist/maplibre-gl.js'),
+	new Promise((p,d)=>
+	{
+		let s=document.createElement('link');
+		s.rel='stylesheet';
+		s.href='https://unpkg.com/maplibre-gl@^5.24.0/dist/maplibre-gl.css';
+		s.onload=p();
+		document.head.appendChild(s);
+	})
+]).then(p=>
 {
 	const d=document.createElement('div');
 	document.body.appendChild(d);
@@ -18,23 +27,23 @@ ss('https://unpkg.com/maplibre-gl@^5.24.0/dist/maplibre-gl.js').then(p=>
 	d.style.width='100dvw';
 	d.style.height='100dvh';
 	const b=new maplibregl.Map({
-	container: "d",
-	style: {
-		version: 8,
-			projection:{'type':'globe'},
-		sources: {
-			"raster-tiles": {
-				type: "raster",
-				tiles: [
-					"https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-					"https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-					"https://mt2.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-					"https://mt3.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-				],
-				tileSize: 256,
+		container: "d",
+		style: {
+			version: 8,
+				projection:{'type':'globe'},
+			sources: {
+				"raster-tiles": {
+					type: "raster",
+					tiles: [
+						"https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+						"https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+						"https://mt2.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+						"https://mt3.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+					],
+					tileSize: 256,
+				},
 			},
+			layers: [{ id: "raster-layer", type: "raster", source: "raster-tiles" }],
 		},
-		layers: [{ id: "raster-layer", type: "raster", source: "raster-tiles" }],
-	},
-});
+	});
 });
