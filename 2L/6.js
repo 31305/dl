6659ae89c55abd16e74d92c96f6955b105f22b21
- <>
2026-06-22 17:17:16 +0530

    -

--- 2L/6.js
+++ 2L/6.js
@@ -10,7 +10,30 @@ const ss=(n)=>
 		document.body.appendChild(v);
 	});
 }
-Promise.all([ss('https://unpkg.com/maplibre-gl@^5.24.0/dist/maplibre-gl.js'),
+const vp=class
+{
+	bs=false;
+	vp=null;
+	dk()
+	{
+		return new Promise((p,d)=>
+		{
+			vm({dk:p,vppk:()=>{this.bs=false;if(this.vp!=null)this.vp()}}).then(p=>this.p=p);
+		});
+	}
+	b(v)
+	{
+		return new Promise((pd,d)=>
+		{
+			if(this.bs||this.p==undefined)d();
+			this.bs=true;
+			this.vp=()=>{this.vp=null;pd();}
+			this.p.ccall('jb',null,['array','number'],[new Uint8Array(v),v.length]);
+		});
+	}
+}
+
+Promise.all([ss('vm.js'),ss('https://unpkg.com/maplibre-gl@^5.24.0/dist/maplibre-gl.js'),
 	new Promise((p,d)=>
 	{
 		let s=document.createElement('link');
@@ -19,7 +42,7 @@ Promise.all([ss('https://unpkg.com/maplibre-gl@^5.24.0/dist/maplibre-gl.js'),
 		s.onload=p();
 		document.head.appendChild(s);
 	})
-]).then(p=>
+]).then(p=>{const v=new vp();v.dk().then(p=>
 {
 	const d=document.createElement('div');
 	document.body.appendChild(d);
@@ -28,7 +51,7 @@ Promise.all([ss('https://unpkg.com/maplibre-gl@^5.24.0/dist/maplibre-gl.js'),
 	d.style.height='100dvh';
 	const b=new maplibregl.Map({
 		attributionControl:false,
-		zoom:2,
+		minZoom:2,
 		container: "d",
 		style: {
 			version: 8,
@@ -56,6 +79,7 @@ Promise.all([ss('https://unpkg.com/maplibre-gl@^5.24.0/dist/maplibre-gl.js'),
 				features: [
 					{
 						type: 'Feature',
+						properties:{n:[49,67,5,70,3,75]},
 						geometry: {
 							type: 'Point',
 							coordinates: [75, 20]
@@ -75,5 +99,12 @@ Promise.all([ss('https://unpkg.com/maplibre-gl@^5.24.0/dist/maplibre-gl.js'),
 		});
 	});
 	b.on('click', p => {console.log(p.lngLat.lng, p.lngLat.lat);});
+	b.on('click','n',p=>
+	{
+		v.b(JSON.parse(p.features[0].properties.n));
+	});
+	b.dragRotate.disable();
+	b.keyboard.disable();
+	b.touchZoomRotate.disableRotation();
 	window.tp=b;
-});
+})});
