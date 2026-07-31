b5b7129077806c92fd346d5a8bb0b17bf3ea0006
- <>
2026-07-31 20:28:36 +0530

    -

--- 2L/l.js
+++ 2L/l.js
@@ -786,7 +786,7 @@ const svvn=class
 			new BABYLON.ExecuteCodeAction({trigger:BABYLON.ActionManager.OnIntersectionEnterTrigger,parameter:lpc},
 				()=>
 				{
-					if(!s.isActiveMesh(this.pk)return;
+					if(!s.isActiveMesh(mm.getChildMeshes()[0]))return;
 					if(!v.bs)v.b(d.vp);
 					psl=(t)=>
 					{
